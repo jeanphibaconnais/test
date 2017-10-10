a8ba2923cb8df340142007b65d88f74d7a29cf1e
JP <B>
2017-10-10 17:33:34 +0200

    Ajout jdk 1.8 + qques tests.

--- java8/src/main/java/java8/Lambda.java
+++ java8/src/main/java/java8/Lambda.java
@@ -1,8 +1,26 @@
 package java8;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Lambda {
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
+
+		testerComparatorList();
 		
+		testerThrad();
+	}
+
+	private static void testerThrad() {
+		Thread monThread = new Thread(() -> { System.out.println("Mon traitement"); });
+		monThread.start();
+	}
+
+	public static void testerComparatorList() {
+		List<String> strings = Arrays.asList("hello", "world", "!");
+		Collections.sort(strings, (s1, s2)-> s1.compareTo(s2));
+		System.out.println(strings);
 	}
 }
