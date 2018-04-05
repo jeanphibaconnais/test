71d6f8bb7cdec1e7871e463a93d0382426f534c2
Hip <jean-philippe.baconnais@pole-emploi.fr>
2018-04-05 11:17:24 +0200

    test connexion bdd mongo

--- graphql/index.js
+++ graphql/index.js
@@ -1,60 +1,66 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
+const { GraphQLServer } = require('graphql-yoga');
+const { rsvp } = require('rsvp');
 
-// Some fake data
-const books = [
-  {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
+var Db = require('mongodb').Db,
+	MongoClient = require('mongodb').MongoClient,
+	Server = require('mongodb').Server,
+	ReplSetServers = require('mongodb').ReplSetServers,
+	ObjectID = require('mongodb').ObjectID,
+	Binary = require('mongodb').Binary,
+	GridStore = require('mongodb').GridStore,
+	Grid = require('mongodb').Grid,
+	Code = require('mongodb').Code,
+	assert = require('assert');
+
+let minisites = [
+	{
+		id: 'link-0',
+		idMinisite: '2343',
+		raisonSociale: 'Fullstack tutorial for GraphQL'
+	}
 ];
+let idCount = minisites.length;
 
-// The GraphQL schema in string form
-const typeDefs = `
-  type Query { getBooks: [Book] }
-  type Book { title: String, author: String }
-`;
+var db = new Db('test', new Server('127.0.0.1', 27017));
+console.log('db connected ? ' + db._state);
 
-// The resolvers
+if (db._state == 'connected') {
+	db.open(function(err, db) {
+		assert.equal(null, err);
+
+		db.on('close', test.done.bind(test));
+		db.close();
+	});
+}
+
+// 2
 const resolvers = {
-  Query: { 
-    getBooks: () => books 
-  },
+	Query: {
+		minisites: () => minisites
+	},
+	// 3
+	Minisite: {
+		id: root => root.id,
+		idMinisite: root => root.idMinisite,
+		raisonSociale: root => root.raisonSociale
+	},
+	Mutation: {
+		// 2
+		post: (root, args) => {
+			const minisite = {
+				id: `link-${idCount++}`,
+				raisonSociale: args.raisonSociale,
+				idMinisite: args.idMinisite
+			};
+			minisites.push(minisite);
+			return minisite;
+		}
+	}
 };
 
-// Mutations
-/*
-const mutations = {
-  addBook : {
-    type : [Book]
-  }
-};
-*/
-
-// Put together a schema
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-  //mutations,
+// 3
+const serverGraphQL = new GraphQLServer({
+	typeDefs: './schema.graphql',
+	resolvers
 });
-
-
-// Initialize the app
-const app = express();
-
-// The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
-// GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-
-// Start the server
-app.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
-});
+serverGraphQL.start(() => console.log(`Server is running on http://localhost:4000`));
